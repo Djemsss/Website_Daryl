f81397ba1bb56d40474a305a5de7597908bb8f88
Daryl Jaimes <daryljaimes88@gmail.com>
2023-02-19 21:59:04 +0100

    Improve NEA-X project with adaptative resolution

--- projects/NEA-X/NEA-X_script.js
+++ projects/NEA-X/NEA-X_script.js
@@ -7,14 +7,22 @@ var eiffelSVG = null
 var personPath = null
 var eiffelPath = null
 
+var global_scale = 1
+
 
 window.addEventListener("resize", (ev) => {
     // adjust canvas to screen
     document.getElementById("canvas").width = window.innerWidth;
     document.getElementById("canvas").height = window.innerHeight;
-
     document.getElementById("canvas2").width = window.innerWidth;
     document.getElementById("canvas2").height = window.innerHeight;
+
+    global_scale = window.innerWidth / 1440
+    if (eiffelSVG){
+      eiffelSVG.style.width = (320 * global_scale) + "px"
+      eiffelSVG.style.height = (320 * global_scale) + "px"
+    }
+    
 });
 
 // Run when window is loaded
@@ -58,82 +66,92 @@ window.addEventListener('load', (event) => {
     eiffelSVG.style.transition = "all 0.5s"
     eiffelSVG.style.scale = "0.5"
 
+    global_scale = window.innerWidth / 1440
+    eiffelSVG.style.width = (320 * global_scale) + "px"
+    eiffelSVG.style.height = (320 * global_scale) + "px"
+
     load_asteroid()
     
 });
 
 function load_asteroid(){
+  // Display loading icon
+  document.getElementById("loadAstButton").classList.add("loading")
+  document.getElementById("loadAstButton").textContent = ""
+
   let randPage = Math.floor(Math.random() * 1598)
-      let randObject = Math.floor(Math.random() * 20)
-      // let link = "https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=XvnPh0wz7AMphlOLFEPqSJBE2cjj0ZTf3xZlFqUv"
-      let link = "http://api.nasa.gov/neo/rest/v1/neo/browse?page=" + randPage + "&size=20&api_key=XvnPh0wz7AMphlOLFEPqSJBE2cjj0ZTf3xZlFqUv"
+  let randObject = Math.floor(Math.random() * 20)
+  // let link = "https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=XvnPh0wz7AMphlOLFEPqSJBE2cjj0ZTf3xZlFqUv"
+  let link = "http://api.nasa.gov/neo/rest/v1/neo/browse?page=" + randPage + "&size=20&api_key=XvnPh0wz7AMphlOLFEPqSJBE2cjj0ZTf3xZlFqUv"
 
-      fetch(link)
-      .then((response) => response.json())
-      .then((fullData) => {
-          // let data = fullData
-          let data = fullData.near_earth_objects[randObject]
-          console.log(data)
-          let id = data.id
-          let designation = data.designation
-          let name = data.name
-          let magnitude = data.absolute_magnitude_h
-          let diameter_min = data.estimated_diameter.meters.estimated_diameter_min
-          let diameter_max = data.estimated_diameter.meters.estimated_diameter_max
-          let diameter_mid = (diameter_min + diameter_max) / 2
+  fetch(link)
+  .then((response) => response.json())
+  .then((fullData) => {
+      // let data = fullData
+      let data = fullData.near_earth_objects[randObject]
+      console.log(data)
+      let id = data.id
+      let designation = data.designation
+      let name = data.name
+      let magnitude = data.absolute_magnitude_h
+      let diameter_min = data.estimated_diameter.meters.estimated_diameter_min
+      let diameter_max = data.estimated_diameter.meters.estimated_diameter_max
+      let diameter_mid = (diameter_min + diameter_max) / 2
 
-          let scaleElementSize = 160
-          if (diameter_mid > 2000){
-            // Scale down eiffel tower a lot
-            scaleElementSize = 40
-            eiffelSVG.style.scale = "0.125"
-          }
-          else if (diameter_mid > 600) {
-            // Scale down eiffel tower
-            scaleElementSize = 80
-            eiffelSVG.style.scale = "0.25"
-          }
-          else if (diameter_mid < 300){
-            // Scale up eiffel tower
-            scaleElementSize = 320
-            eiffelSVG.style.scale = "1"
-          }
+      let scaleElementSize = 160
+      if (diameter_mid > 2000){
+        // Scale down eiffel tower a lot
+        scaleElementSize = 40
+        eiffelSVG.style.scale = "0.125"
+      }
+      else if (diameter_mid > 600) {
+        // Scale down eiffel tower
+        scaleElementSize = 80
+        eiffelSVG.style.scale = "0.25"
+      }
+      else if (diameter_mid < 300){
+        // Scale up eiffel tower
+        scaleElementSize = 320
+        eiffelSVG.style.scale = "1"
+      }
 
-          
+      
 
-          let hazardous = data.is_potentially_hazardous_asteroid
+      let hazardous = data.is_potentially_hazardous_asteroid
 
-          // Orbit details
-          let aphelion = data.orbital_data.aphelion_distance
-          let perihelion = data.orbital_data.perihelion_distance
-          let scale = remap(aphelion, 1, 5, 80, 20)
-          
-          let eccentricity = data.orbital_data.eccentricity
-          let inclination = data.orbital_data.inclination
-          let asc_node = data.orbital_data.ascending_node_longitude
-          let orbital_period = data.orbital_data.orbital_period
+      // Orbit details
+      let aphelion = data.orbital_data.aphelion_distance
+      let perihelion = data.orbital_data.perihelion_distance
+      let scale = remap(aphelion, 1, 5, 80, 20)
+      
+      let eccentricity = data.orbital_data.eccentricity
+      let inclination = data.orbital_data.inclination
+      let asc_node = data.orbital_data.ascending_node_longitude
+      let orbital_period = data.orbital_data.orbital_period
 
-          set_right_display(id, designation, name, magnitude, diameter_min, diameter_max, diameter_mid, hazardous, aphelion, perihelion, eccentricity, inclination, orbital_period)
+      set_right_display(id, designation, name, magnitude, diameter_min, diameter_max, diameter_mid, hazardous, aphelion, perihelion, eccentricity, inclination, orbital_period)
 
-          set_asteroid_size(diameter_min, diameter_max, diameter_mid, scaleElementSize)
+      set_asteroid_size(diameter_min, diameter_max, diameter_mid, scaleElementSize)
 
-          // Clear canvas and redraw orbits
-          const context = document.getElementById("canvas").getContext("2d");
-          context.clearRect(0, 0, canvas.width, canvas.height);
+      // Clear canvas and redraw orbits
+      const context = document.getElementById("canvas").getContext("2d");
+      context.clearRect(0, 0, canvas.width, canvas.height);
 
-          drawOrbit(document.getElementById("canvas"), perihelion , aphelion, eccentricity, inclination, asc_node, scale, "rgba(255, 0, 0, 255)")
-          drawOrbit(document.getElementById("canvas"), 0.983, 1.016, 0.0167, 7, 175, scale, "rgba(0, 0, 255, 255)")
+      drawOrbit(document.getElementById("canvas"), perihelion , aphelion, eccentricity, inclination, asc_node, scale, "rgba(255, 0, 0, 255)")
+      drawOrbit(document.getElementById("canvas"), 0.983, 1.016, 0.0167, 7, 175, scale, "rgba(0, 0, 255, 255)")
 
-
-      });
+      // Hide loading icon
+      document.getElementById("loadAstButton").classList.remove("loading")
+      document.getElementById("loadAstButton").textContent = "New Asteroid"
+  });
 }
 
 function set_asteroid_size(diameter_min, diameter_max, diameter_mid, scaleElementSize = 160){
-    let ratio = 330 / scaleElementSize
+    let ratio = 330 / scaleElementSize * global_scale
 
-    let diameter_min_pixels = diameter_min / ratio
-    let diameter_max_pixels = diameter_max / ratio
-    let diameter_mid_pixels = diameter_mid / ratio
+    let diameter_min_pixels = (diameter_min / ratio) * global_scale
+    let diameter_max_pixels = (diameter_max / ratio) * global_scale
+    let diameter_mid_pixels = (diameter_mid / ratio) * global_scale
 
     // Set asteroid sizes
     if (true){
@@ -163,27 +181,28 @@ function set_asteroid_size(diameter_min, diameter_max, diameter_mid, scaleElemen
 }
 
 function set_right_display(id, designation, name, magnitude, diameter_min, diameter_max, diameter_mid, hazardous, aphelion, perihelion, eccentricity, inclination, orbital_period){
-    document.getElementById("asteroidID").textContent = "ID: " + id
-    document.getElementById("asteroidDesignation").textContent = "Designation: " + designation
-    document.getElementById("asteroidName").textContent = "Name: " + name
-    document.getElementById("asteroidMagnitude").textContent = "Magnitude: " + magnitude
-    document.getElementById("asteroidDiaMin").textContent = "Diameter min: " + Math.floor(diameter_min) + " meters"
-    document.getElementById("asteroidDiaMax").textContent = "Diameter max: " + Math.floor(diameter_max) + " meters"
-    document.getElementById("asteroidDiaMid").textContent = "Diameter average: " + Math.floor(diameter_mid) + " meters"
-    document.getElementById("asteroidHazardous").textContent = "Hazardous: " + hazardous
-    document.getElementById("asteroidApo").textContent = "Aphelion: " + parseFloat(aphelion).toFixed(2) + " A.U."
-    document.getElementById("asteroidPeri").textContent = "Perihelion: " + parseFloat(perihelion).toFixed(2) + " A.U."
-    document.getElementById("asteroidEccentricity").textContent = "Eccentricity: " + eccentricity
-    document.getElementById("asteroidInclination").textContent = "Inclination: " + parseFloat(inclination).toFixed(2) + "°"
-    document.getElementById("asteroidPeriod").textContent = "Orbital Period: " + parseFloat(orbital_period).toFixed(2) + " days"
+    document.getElementById("asteroidID").textContent = id
+    document.getElementById("asteroidDesignation").textContent = designation
+    document.getElementById("asteroidName").textContent = name
+    document.getElementById("asteroidMagnitude").textContent = magnitude
+    document.getElementById("asteroidDiaMin").textContent = Math.floor(diameter_min) + " meters"
+    document.getElementById("asteroidDiaMax").textContent = Math.floor(diameter_max) + " meters"
+    document.getElementById("asteroidDiaMid").textContent = Math.floor(diameter_mid) + " meters"
+    document.getElementById("asteroidHazardous").textContent = hazardous
+    document.getElementById("asteroidApo").textContent = parseFloat(aphelion).toFixed(3) + " A.U."
+    document.getElementById("asteroidPeri").textContent = parseFloat(perihelion).toFixed(3) + " A.U."
+    document.getElementById("asteroidEccentricity").textContent = parseFloat(eccentricity).toFixed(3)
+    document.getElementById("asteroidInclination").textContent = parseFloat(inclination).toFixed(3) + "°"
+    document.getElementById("asteroidPeriod").textContent = parseFloat(orbital_period).toFixed(3) + " days"
 }
 
 function drawOrbit(canvas, perigee, apogee, eccentricity, inclination, ascendingNode, scale = 80, color) {
+  console.log(global_scale)
   const context = canvas.getContext("2d");
   const sun = document.getElementById("sun");
   const sunX = (sun.offsetLeft + sun.offsetWidth / 2) + document.getElementById("graphicsBox").getBoundingClientRect().x;
   const sunY = (sun.offsetTop + sun.offsetHeight / 2) + 100;
-  const semiMajorAxis = (perigee * scale + apogee * scale) / 2;
+  const semiMajorAxis = (perigee * scale * global_scale + apogee * scale * global_scale) / 2;
   const semiMinorAxis = semiMajorAxis * Math.sqrt(1 - eccentricity ** 2);
   const centerOffset = semiMajorAxis * eccentricity;
   const centerX = sunX + centerOffset * Math.cos(ascendingNode * Math.PI / 180);
@@ -195,6 +214,7 @@ function drawOrbit(canvas, perigee, apogee, eccentricity, inclination, ascending
 
 
   context.beginPath();
+  context.lineWidth = 2 * global_scale;
   context.ellipse(centerX, centerY, semiMajorAxis, semiMinorAxis, ascendingNode * Math.PI / 180, 0, 2 * Math.PI);
   context.strokeStyle = color;
   context.stroke();
